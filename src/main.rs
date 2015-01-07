426250de19a8d9111d8e08e01662f7f2c0528b5d
Kevin Baribeau <kevin.baribeau@gmail.com>
2015-01-07 10:59:11 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+extern crate regex;
+
+struct Monster {
+    health: i32,
+    strength: i32,
+}
+
+trait Attack {
+    fn attack(&self, target: &mut Monster);
+}
+
+impl Attack for Monster {
+    fn attack(&self, target: &mut Monster) {
+        target.health -= self.strength;
+    }
+}
+
+fn main() {
+    let hero = Monster {
+        health: 5,
+        strength: 10
+    };
+
+    let enemy = &mut Monster {
+        health: 25,
+        strength: 1
+    };
+
+    println!("the enemy's health is {}", enemy.health);
+    hero.attack(enemy);
+    println!("the enemy's health is {}", enemy.health);
+}
